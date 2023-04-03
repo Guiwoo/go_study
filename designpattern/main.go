592d9d0539c359d20611b5f937cf048a1024e500
Guiwoo <park.guiwoo@hotmail.com>
2023-04-04 00:26:18 +0900

    prototype with encode decode way

---
+++ designpattern/main.go
@@ -0,0 +1,13 @@
+package designpattern
+
+/**
+Complicated object ts aren't designed from scratch
+- They reiterate exisitng desings
+
+An existing desing is a Prototype
+We make a copy of the prototype and customize it
+Requires 'deep copy' support
+We make the cloning convenient via a Factory
+*/
+
+// Deep copyting
---
+++ designpattern/prototype/prototype.go
@@ -0,0 +1,108 @@
+package prototype
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+)
+
+type address struct {
+	streetAddress, city, country string
+}
+
+type person struct {
+	name    string
+	address *address
+	friends []string
+}
+
+func deepCopyingProblem() {
+	john := &person{
+		name:    "John",
+		address: &address{"123 london rd", "london", "uik"},
+	}
+
+	//jane := john
+	//jane.name = "Jane"
+	//jane.address.streetAddress = "321 Baker street"
+
+	jane := john
+	jane.address = &address{
+		john.address.streetAddress,
+		john.address.city,
+		john.address.country,
+	}
+
+	jane.address.streetAddress = "321 Baker street"
+
+	// deep copying when you copying object , pointer ,slice copy the original object
+	// it doesn't affect for the copy the object it might occur the problem to pointer copies
+}
+
+// make a receiver function
+func (a *address) DeepCopy() *address {
+	return &address{}
+}
+
+// slice ,string, map still needs copy triple and double check the type not idle way
+// one possible of approach to handle deep copy
+
+func (p *person) DeepCopy() *person {
+	q := p
+	q.address = p.address.DeepCopy()
+	copy(q.friends, p.friends)
+	return q
+}
+
+func (p *person) DeepCopyUtil() *person {
+	b := bytes.Buffer{}
+	e := gob.NewEncoder(&b)
+	_ = e.Encode(p)
+
+	fmt.Println(string(b.Bytes()))
+
+	d := gob.NewDecoder(&b)
+	result := &person{}
+	_ = d.Decode(result)
+
+	return result
+}
+
+/**
+Prototype factory
+*/
+
+type employee struct {
+	name   string
+	office address
+}
+
+func (p *employee) DeepCopy() *employee {
+	b := bytes.Buffer{}
+	e := gob.NewEncoder(&b)
+	_ = e.Encode(p)
+
+	d := gob.NewDecoder(&b)
+	result := &employee{}
+	_ = d.Decode(result)
+
+	return result
+}
+
+var mainOffice = employee{"", address{"0", "123 East Dr", "London"}}
+var auxOffice = employee{"", address{"0", "66 West Dr", "London"}}
+
+func newEmployee(proto *employee, name, city string) *employee {
+	result := proto.DeepCopy()
+	result.name = name
+	result.office.city = city
+	return result
+}
+
+func newMainOfficeEmployee(name, city string) *employee {
+	return newEmployee(&mainOffice, name, city)
+}
+
+func newAuxOfficeEmployee(name, city string) *employee {
+	return newEmployee(&auxOffice, name, city)
+}
