3309892c9ee56c8063df280aa369b75c19e2c87e
Guiwoo <park.guiwoo@hotmail.com>
2023-05-11 13:48:36 +0900

    proxy and leetcode

--- leetcode/main.go
+++ leetcode/main.go
@@ -2,67 +2,43 @@ package main
 
 import "fmt"
 
-type Point struct {
-	row, col int
-	dir      []int
+func maxUncrossedLines(nums1 []int, nums2 []int) int {
+	m, n := len(nums1), len(nums2)
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+
+	for i, num1 := range nums1 {
+		for j, num2 := range nums2 {
+			if num1 == num2 {
+				dp[i+1][j+1] = dp[i][j] + 1
+			} else {
+				dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+			}
+		}
+	}
+
+	return dp[m][n]
 }
 
-func generateMatrix(n int) [][]int {
-	answer := make([][]int, n)
-	for i := range answer {
-		answer[i] = make([]int, n)
+func max(a, b int) int {
+	if a > b {
+		return a
 	}
-
-	cur := &Point{0, 0, []int{0, 1}}
-
-	for i := 0; i < n*n; i++ {
-		num := i + 1
-		validate(cur, answer)
-		fil(cur, answer, num)
-	}
-
-	return answer
-}
-
-func fil(cur *Point, board [][]int, num int) {
-	board[cur.row][cur.col] = num
-}
-
-func validate(cur *Point, board [][]int) {
-	if board[cur.row][cur.col] == 0 {
-		return
-	}
-	nextRow := cur.row + cur.dir[0]
-	nextCol := cur.col + cur.dir[1]
-
-	if nextRow < 0 || nextRow >= len(board) || nextCol < 0 || nextCol >= len(board[0]) || board[nextRow][nextCol] != 0 {
-		cur.dir = getNextDir(cur.dir)
-	}
-
-	cur.row = cur.row + cur.dir[0]
-	cur.col = cur.col + cur.dir[1]
-}
-
-func getNextDir(dir []int) (ans []int) {
-	switch true {
-	case dir[0] == 0 && dir[1] == 1:
-		//오른쪽
-		ans = []int{1, 0}
-	case dir[0] == 1 && dir[1] == 0:
-		//아래로
-		ans = []int{0, -1}
-	case dir[0] == 0 && dir[1] == -1:
-		//왼쪽
-		ans = []int{-1, 0}
-	default:
-		ans = []int{0, 1}
-	}
-	return ans
+	return b
 }
 
 func main() {
-	board := generateMatrix(2)
-	for _, v := range board {
-		fmt.Println(v)
-	}
+	answer := maxUncrossedLines([]int{1, 4, 2}, []int{1, 2, 4})
+	fmt.Println(answer)
+
+	answer = maxUncrossedLines([]int{2, 5, 1, 2, 5}, []int{10, 5, 2, 1, 5, 2})
+	fmt.Println(answer)
+
+	answer = maxUncrossedLines([]int{1, 3, 7, 1, 7, 5}, []int{1, 9, 2, 5, 1})
+	fmt.Println(answer)
+
+	answer = maxUncrossedLines([]int{1, 2, 3, 4, 5, 6}, []int{6, 2, 3, 4, 5, 1})
+	fmt.Println(answer)
 }
