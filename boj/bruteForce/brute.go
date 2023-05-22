6191325c7390645c2c3402050887287cf2c9709f
Guiwoo <park.guiwoo@hotmail.com>
2023-05-22 14:03:40 +0900

    bit_mask_way

--- boj/bruteForce/brute.go
+++ boj/bruteForce/brute.go
@@ -142,3 +142,44 @@ func toArr(sc *bufio.Scanner) []string {
 	rs := strings.Split(input, " ")
 	return rs
 }
+
+func ps_03_02() {
+	sc := bufio.NewScanner(os.Stdin)
+
+	params := toArr(sc)
+	arrLen, _ := strconv.Atoi(params[0])
+	target, _ := strconv.Atoi(params[1])
+
+	arr := make([]int, arrLen)
+	nums := toArr(sc)
+	for i, numStr := range nums {
+		arr[i], _ = strconv.Atoi(numStr)
+	}
+
+	answer := subArray_02(arr, target)
+	if target == 0 {
+		answer--
+	}
+	fmt.Println(answer)
+}
+
+func subArray_02(arr []int, target int) int {
+	answer := 0
+	n := len(arr)
+
+	// 비트 마스크를 0부터 (2^n - 1)까지 순회합니다.
+	for i := 0; i < (1 << n); i++ {
+		sum := 0
+		// 비트 마스크의 각 비트를 확인하여 포함된 원소의 합을 계산합니다.
+		for j := 0; j < n; j++ {
+			if (i & (1 << j)) > 0 {
+				sum += arr[j]
+			}
+		}
+		if sum == target {
+			answer++
+		}
+	}
+
+	return answer
+}
--- boj/main.go
+++ boj/main.go
@@ -1,10 +1,57 @@
 package main
 
-/**
-5 0
--7 -3 -2 5 8
-*/
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
+	sc := bufio.NewScanner(os.Stdin)
 
+	params := toArr(sc)
+	arrLen, _ := strconv.Atoi(params[0])
+	target, _ := strconv.Atoi(params[1])
+
+	arr := make([]int, arrLen)
+	nums := toArr(sc)
+	for i, numStr := range nums {
+		arr[i], _ = strconv.Atoi(numStr)
+	}
+
+	answer := subArray(arr, target)
+	if target == 0 {
+		answer--
+	}
+	fmt.Println(answer)
+}
+
+func subArray(arr []int, target int) int {
+	answer := 0
+	n := len(arr)
+
+	// 비트 마스크를 0부터 (2^n - 1)까지 순회합니다.
+	for i := 0; i < (1 << n); i++ {
+		sum := 0
+		// 비트 마스크의 각 비트를 확인하여 포함된 원소의 합을 계산합니다.
+		for j := 0; j < n; j++ {
+			if (i & (1 << j)) > 0 {
+				sum += arr[j]
+			}
+		}
+		if sum == target {
+			answer++
+		}
+	}
+
+	return answer
+}
+
+func toArr(sc *bufio.Scanner) []string {
+	sc.Scan()
+	input := sc.Text()
+	rs := strings.Split(input, " ")
+	return rs
 }
