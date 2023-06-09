9f1422b03843bb96ba4d03bb83447e606211c7a2
Guiwoo <park.guiwoo@hotmail.com>
2023-06-09 11:28:29 +0900

    고 동시성 프로그래밍 제한 패턴

--- concurrency/con_pattern_test.go
+++ concurrency/con_pattern_test.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 	"testing"
+	"time"
 )
 
 //제한
@@ -69,5 +71,68 @@ func Test_Pattern_03(t *testing.T) {
 }
 
 func Test_Pattern_04(t *testing.T) {
-	dowork := func(strings <- chan string)
+	dowork := func(strings <-chan string) <-chan interface{} {
+		completed := make(chan interface{})
+		go func() {
+			defer close(completed)
+			defer fmt.Println("Done")
+			for s := range strings {
+				fmt.Println(s)
+			}
+		}()
+		return completed
+	}
+
+	dowork(nil)
+	fmt.Println("Done.")
+}
+
+func Test_Pattern_05(t *testing.T) {
+	dowork := func(done <-chan bool, strings <-chan string) <-chan interface{} {
+		terminated := make(chan interface{})
+		go func() {
+			defer fmt.Println("dowork exited")
+			//defer close(terminated)
+			for {
+				select {
+				case s := <-strings:
+					fmt.Printf("Received : %s\n", s)
+				case <-done:
+					return
+				}
+			}
+		}()
+		return terminated
+	}
+
+	done := make(chan bool)
+	terminated := dowork(done, nil)
+	go func() {
+		time.Sleep(1 * time.Second)
+		fmt.Println("Canceling dowork goroutine...")
+		close(done)
+	}()
+
+	<-terminated
+	fmt.Println("Done.")
+}
+
+func Test_Pattern_06(t *testing.T) {
+	newRandStream := func() <-chan int {
+		randStream := make(chan int)
+		go func() {
+			defer fmt.Println("newRandStream closure exited.")
+			defer close(randStream)
+			for {
+				randStream <- rand.Int()
+			}
+		}()
+		return randStream
+	}
+
+	randStream := newRandStream()
+	fmt.Println("3 random ints :")
+	for i := 0; i <= 3; i++ {
+		fmt.Printf("%d : %d\n", i, <-randStream)
+	}
 }
