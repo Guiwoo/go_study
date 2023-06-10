5d8a0cb14e99f9313a4a7733104883fb6eec39a3
Guiwoo <park.guiwoo@hotmail.com>
2023-06-10 21:02:13 +0900

    동시성 프로그래밍 or 패턴, 에러 핸들링 패턴

--- concurrency/con_pattern_test.go
+++ concurrency/con_pattern_test.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
+	"net/http"
 	"sync"
 	"testing"
 	"time"
@@ -135,4 +137,148 @@ func Test_Pattern_06(t *testing.T) {
 	for i := 0; i <= 3; i++ {
 		fmt.Printf("%d : %d\n", i, <-randStream)
 	}
+}
+
+func Test_Pattern_07(t *testing.T) {
+	// 1개 이상의 채널들을 보낼수 있다 <- 보내는 애들로만
+	var or func(channels ...<-chan interface{}) <-chan interface{}
+	or = func(channels ...<-chan interface{}) <-chan interface{} {
+		switch len(channels) {
+		case 0:
+			return nil
+		case 1:
+			return channels[0]
+		}
+
+		orDone := make(chan interface{})
+
+		go func() {
+			defer close(orDone)
+			switch len(channels) {
+			case 2:
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				}
+			default:
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				case <-channels[2]:
+				case <-or(append(channels[3:], orDone)...):
+				}
+			}
+		}()
+		return orDone
+	}
+
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+
+	fmt.Printf("done after %v", time.Since(start))
+}
+
+func Test_Pattern_08(t *testing.T) {
+	checkStatus := func(
+		done <-chan interface{},
+		urls ...string,
+	) <-chan *http.Response {
+		response := make(chan *http.Response)
+		go func() {
+			defer close(response)
+			for _, url := range urls {
+				resp, err := http.Get(url)
+				if err != nil {
+					log.Fatal(err)
+					continue
+				}
+				select {
+				case <-done:
+					return
+				case response <- resp:
+				}
+			}
+		}()
+		return response
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	urls := []string{"https://www.google.com", "https://www.naver.com", "http://localhost:8080"}
+	for response := range checkStatus(done, urls...) {
+		fmt.Printf("Response : %v\n", response.Status)
+	}
+}
+
+func Test_Pattern_09(t *testing.T) {
+	type Result struct {
+		Error    error
+		Response *http.Response
+	}
+
+	checkStatus := func(done <-chan interface{}, urls ...string) <-chan Result {
+		results := make(chan Result)
+		go func() {
+			defer close(results)
+			for _, url := range urls {
+				resp, err := http.Get(url)
+				rs := Result{Error: err, Response: resp}
+				select {
+				case <-done:
+					return
+				case results <- rs:
+				}
+			}
+		}()
+		return results
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+	/*
+		urls := []string{"https://www.google.com", "https://www.naver.com", "http://localhost:8080"}
+		for result := range checkStatus(done, urls...) {
+			if result.Error != nil {
+				fmt.Printf("Error : %v\n", result.Error)
+				continue
+			}
+			fmt.Printf("Response : %v\n", result.Response.Status)
+		}
+	*/
+
+	urls := []string{"https://www.google.com", "https://www.naver.com", "http://localhost:8080", "a", "b", "c", "d", "e", "f", "g"}
+	errCount := 0
+	for result := range checkStatus(done, urls...) {
+		if result.Error != nil {
+			fmt.Printf("Error : %v\n", result.Error)
+			errCount++
+			if errCount >= 3 {
+				fmt.Println("Too many errors, breaking!")
+				break
+			}
+			continue
+		}
+		fmt.Printf("Response : %v\n", result.Response.Status)
+	}
+
+}
+
+func Test_Pattern_10(t *testing.T) {
+
 }
--- socket/main.go
+++ socket/main.go
@@ -1,5 +1,80 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
+var (
+	upgrader = websocket.Upgrader{}
+)
+
+func writer(ws *websocket.Conn, id string) {
+	defer func() {
+		ws.Close()
+	}()
+
+	for {
+		select {}
+	}
+}
+
+type Connection struct {
+	name string
+}
+
+var connections = make(map[string][]*Connection)
+
+type Req struct {
+	Name string `query:"name"`
+}
+
+func hello(c echo.Context) error {
+	id := c.Param("id")
+	req := &Req{}
+	if err := c.Bind(req); err != nil {
+		c.Logger().Error(err)
+	}
+	fmt.Println(req)
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		return err
+	}
+	defer ws.Close()
+
+	for {
+		val, ok := connections[id]
+		if !ok {
+			fmt.Println("There is no room will create")
+			connections[id] = make([]*Connection, 0)
+			connections[id] = append(connections[id], &Connection{name: req.Name})
+		} else {
+			fmt.Printf("There is room and there are %d,\n", len(val))
+			val = append(val, &Connection{name: req.Name})
+			connections[id] = val
+		}
+
+		ws.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("%s has entered Current room user is %d", req.Name, len(connections[id]))))
+
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			c.Logger().Error(err)
+		}
+		fmt.Printf("%s\n", msg)
+
+		defer func() {
+			delete(connections, id)
+		}()
+	}
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/ws/:id", hello)
+	e.Logger.Fatal(e.Start(":1323"))
 }
