b50739b50f961c7cf0c101555d185cf6ef3bc556
Guiwoo <park.guiwoo@hotmail.com>
2023-07-23 15:18:46 +0900

    boj 15650

--- boj/main.go
+++ boj/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	solution()
 }
 
 func solution() {
@@ -13,10 +17,37 @@ func solution() {
 	writer := bufio.NewWriter(os.Stdout)
 	defer writer.Flush()
 
-	_, err := reader.ReadString('\n')
+	input, err := reader.ReadString('\n')
 	if err != nil {
 		panic(err)
 	}
+	inputs := strings.Split(input, " ")
+	n, _ := strconv.Atoi(inputs[0])
+	m, _ := strconv.Atoi(strings.TrimSpace(inputs[1]))
 
-	writer.WriteString("")
+	result := boj15650(n, m)
+	writer.WriteString(result)
+}
+
+func boj15650(n, m int) string {
+	sb := strings.Builder{}
+	out := make([]int, m)
+	recur(n, 0, 0, 0, out, &sb)
+	return sb.String()
+}
+
+func recur(n, start, depth, flag int, out []int, sb *strings.Builder) {
+	if depth == len(out) {
+		result := strings.NewReplacer("[", "", "]", "").Replace(fmt.Sprintf("%v", out))
+		sb.WriteString(result + "\n")
+		return
+	} else {
+		for i := start; i < n; i++ {
+			if flag&(1<<i) != 0 {
+				continue
+			}
+			out[depth] = i + 1
+			recur(n, i+1, depth+1, flag|(1<<i), out, sb)
+		}
+	}
 }
