5e91494a9c1b68632fa307db61483f8c45684d7a
Guiwoo <park.guiwoo@hotmail.com>
2023-07-26 00:41:21 +0900

    20230725

--- boj/main.go
+++ boj/main.go
@@ -24,6 +24,31 @@ func solution() {
 	inputs := strings.Split(input, " ")
 	n, _ := strconv.Atoi(inputs[0])
 	m, _ := strconv.Atoi(strings.TrimSpace(inputs[1]))
+	//writer.WriteString(fmt.Sprintf("%d  %d", n, m))
+	writer.WriteString(boj15652(n, m))
+}
 
-	writer.WriteString(fmt.Sprintf("%d  %d", n, m))
+func boj15652(n, m int) string {
+	sb := strings.Builder{}
+	out := make([]int, m)
+	permutation(n, 0, out, &sb)
+	return sb.String()
+}
+func permutation(n, depth int, out []int, sb *strings.Builder) {
+	if depth == len(out) {
+		for _, v := range out {
+			sb.WriteString(fmt.Sprintf("%d ", v))
+		}
+		sb.WriteString("\n")
+		return
+	} else {
+		start := 0
+		if depth != 0 {
+			start = out[depth-1] - 1
+		}
+		for i := start; i < n; i++ {
+			out[depth] = i + 1
+			permutation(n, depth+1, out, sb)
+		}
+	}
 }
