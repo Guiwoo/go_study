75a1ea1c7c71077998f221a5308219f61fa7de9a
Guiwoo <park.guiwoo@hotmail.com>
2023-08-01 12:52:37 +0900

    boj 15661 fail to solve

--- echo_proxy/main.go
+++ echo_proxy/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"crypto/tls"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
-	"golang.org/x/crypto/acme"
 	"net/http"
-	"net/url"
 )
 
 var (
@@ -29,26 +26,19 @@ func main() {
 			Message string `json:"message"`
 		}{"testing", "From Server !"})
 	})
-
-	url1, _ := url.Parse("http://localhost:9096")
-	transport := &http.Transport{
-		TLSClientConfig: &tls.Config{
-			InsecureSkipVerify: true,
-		},
-	}
-	grp := e.Group(currentServer)
-	grp.Use(middleware.Recover())
-	grp.Use(middleware.ProxyWithConfig(middleware.ProxyConfig{
-		Balancer:  middleware.NewRoundRobinBalancer([]*middleware.ProxyTarget{{URL: url1}}),
-		Transport: transport,
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{"*"},
 	}))
+	//s := http.Server{
+	//	Addr:    ":9098",
+	//	Handler: e,
+	//	TLSConfig: &tls.Config{
+	//		NextProtos: []string{acme.ALPNProto},
+	//	},
+	//}
+	//log.Error(s.ListenAndServeTLS(serverCert, serverKey))
 
-	s := http.Server{
-		Addr:    ":9098",
-		Handler: e,
-		TLSConfig: &tls.Config{
-			NextProtos: []string{acme.ALPNProto},
-		},
-	}
-	log.Error(s.ListenAndServeTLS(cert, key))
+	http.Get("https://im.plea.kr:9098/test")
+	log.Error(e.Start(":9098"))
 }
