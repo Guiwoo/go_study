3f8a3ffcffb6d6664c8739704a95d2ee92305b7e
Guiwoo <park.guiwoo@hotmail.com>
2023-10-08 17:13:14 +0900

    boj bfs

--- boj/dfs_bfs/dfs_bfs.go
+++ boj/dfs_bfs/dfs_bfs.go
@@ -223,3 +223,61 @@ func boj11123Dfs(arr [][]string) string {
 	}
 	return strconv.Itoa(ans)
 }
+
+func boj1012() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var cases int
+	fmt.Fscanln(reader, &cases)
+
+	for i := 0; i < cases; i++ {
+		var row, col, lines int
+		fmt.Fscanln(reader, &row, &col, &lines)
+
+		arr := make([][]int, row)
+		for r := range arr {
+			arr[r] = make([]int, col)
+		}
+
+		for j := 0; j < lines; j++ {
+			var tRow, tCol int
+			fmt.Fscanln(reader, &tRow, &tCol)
+			arr[tRow][tCol] = 1
+		}
+
+		writer.WriteString(boj1012Bfs(arr) + "\n")
+	}
+}
+
+func boj1012Bfs(arr [][]int) string {
+	dirs := []int{0, 1, 0, -1, 0}
+	var ans int
+
+	for i := range arr {
+		for j := range arr[i] {
+			if arr[i][j] == 1 {
+				ans++
+				arr[i][j] = -1
+				q := [][]int{{i, j}}
+				for len(q) > 0 {
+					cur := q[0]
+					q = q[1:]
+					for k := 1; k < len(dirs); k++ {
+						nRow := cur[0] + dirs[k-1]
+						nCol := cur[1] + dirs[k]
+
+						if nRow < 0 || nCol < 0 || nRow >= len(arr) || nCol >= len(arr[i]) || arr[nRow][nCol] < 1 {
+							continue
+						}
+						arr[nRow][nCol] = -1
+						q = append(q, []int{nRow, nCol})
+					}
+				}
+			}
+		}
+	}
+
+	return strconv.Itoa(ans)
+}
--- boj/main.go
+++ boj/main.go
@@ -4,67 +4,84 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
+	"sort"
 )
 
 func main() {
 	solution()
 }
 
+/**
+7
+0110100
+0110101
+1110101
+0000111
+0100000
+0111110
+0111000
+*/
+
 func solution() {
 	reader := bufio.NewReader(os.Stdin)
-	writer := bufio.NewWriter(os.Stdout)
-	defer writer.Flush()
 
-	var cases int
-	fmt.Fscanln(reader, &cases)
+	var num int
+	fmt.Fscanln(reader, &num)
 
-	for i := 0; i < cases; i++ {
-		var row, col, lines int
-		fmt.Fscanln(reader, &row, &col, &lines)
-
-		arr := make([][]int, row)
-		for r := range arr {
-			arr[r] = make([]int, col)
+	arr := make([][]int, num)
+	for i := range arr {
+		arr[i] = make([]int, num)
+		var input string
+		fmt.Fscanln(reader, &input)
+		for j := 0; j < len(input); j++ {
+			arr[i][j] = int(input[j] - '0')
 		}
-
-		for j := 0; j < lines; j++ {
-			var tRow, tCol int
-			fmt.Fscanln(reader, &tRow, &tCol)
-			arr[tRow][tCol] = 1
-		}
-
-		writer.WriteString(bfs(arr) + "\n")
 	}
-}
 
-func bfs(arr [][]int) string {
-	dirs := []int{0, 1, 0, -1, 0}
-	var ans int
+	var (
+		ans    int
+		amount []int
+	)
 
 	for i := range arr {
 		for j := range arr[i] {
 			if arr[i][j] == 1 {
 				ans++
-				arr[i][j] = -1
-				q := [][]int{{i, j}}
-				for len(q) > 0 {
-					cur := q[0]
-					q = q[1:]
-					for k := 1; k < len(dirs); k++ {
-						nRow := cur[0] + dirs[k-1]
-						nCol := cur[1] + dirs[k]
-
-						if nRow < 0 || nCol < 0 || nRow >= len(arr) || nCol >= len(arr[i]) || arr[nRow][nCol] < 1 {
-							continue
-						}
-						arr[nRow][nCol] = -1
-						q = append(q, []int{nRow, nCol})
-					}
-				}
+				arr[i][j] = 0
+				amount = append(amount, bfs(i, j, arr))
 			}
 		}
 	}
 
-	return strconv.Itoa(ans)
+	fmt.Println(ans)
+	sort.Ints(amount)
+	for _, v := range amount {
+		fmt.Println(v)
+	}
+}
+
+func bfs(row, col int, arr [][]int) int {
+	var ans int
+	dirs := []int{0, 1, 0, -1, 0}
+
+	q := [][]int{{row, col}}
+
+	for len(q) > 0 {
+		cur := q[0]
+		ans++
+		q = q[1:]
+		for i := 1; i < len(dirs); i++ {
+			nRow := cur[0] + dirs[i-1]
+			nCol := cur[1] + dirs[i]
+
+			if nRow < 0 || nCol < 0 || nRow >= len(arr) || nCol >= len(arr[0]) || arr[nRow][nCol] != 1 {
+				continue
+			}
+			arr[nRow][nCol] = 0
+			q = append(q, []int{nRow, nCol})
+		}
+
+	}
+
+	return ans
 }
