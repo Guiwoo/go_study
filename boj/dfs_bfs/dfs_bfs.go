c6ad38b4297fffd6e1fffd631cce0642e1af07fb
Guiwoo <park.guiwoo@hotmail.com>
2023-11-27 13:01:32 +0900

    boj dfs bfs

--- boj/dfs_bfs/dfs_bfs.go
+++ boj/dfs_bfs/dfs_bfs.go
@@ -728,3 +728,61 @@ func setArea(row int, col int, reader *bufio.Reader) [][]rune {
 	}
 	return area
 }
+
+func solution14615() {
+	var (
+		areas, edges, input int
+		reader              = bufio.NewReader(os.Stdin)
+		ans                 = strings.Builder{}
+		visit               = [2][100001]bool{}
+	)
+
+	fmt.Fscanln(reader, &areas, &edges)
+	graph, reverse := setGraph(areas, edges, reader)
+
+	findWay(1, areas, 0, graph, visit)
+	findWay(areas, 1, 1, reverse, visit)
+
+	fmt.Fscanln(reader, &input)
+	for i := 0; i < input; i++ {
+		var bomb int
+		fmt.Fscanln(reader, &bomb)
+		if visit[0][bomb] && visit[1][bomb] {
+			ans.WriteString("Defend the CTP\n")
+		} else {
+			ans.WriteString("Destroyed the CTP\n")
+		}
+	}
+	fmt.Println(ans.String())
+}
+func findWay(from, to, way int, graph map[int][]int, visit [2][100001]bool) {
+	visit[way][from] = true
+	q := []int{from}
+	for len(q) > 0 {
+		cur := q[0]
+		q = q[1:]
+		for i := 0; i < len(graph[cur]); i++ {
+			next := graph[cur][i]
+			if visit[way][next] == false {
+				visit[way][next] = true
+				q = append(q, next)
+			}
+		}
+	}
+	return
+}
+
+func setGraph(areas, edges int, reader *bufio.Reader) (map[int][]int, map[int][]int) {
+	graph := make(map[int][]int)
+	reverse := make(map[int][]int)
+	for i := 0; i <= areas; i++ {
+		graph[i] = make([]int, 0)
+	}
+	for i := 0; i < edges; i++ {
+		var from, to int
+		fmt.Fscanln(reader, &from, &to)
+		graph[from] = append(graph[from], to)
+		reverse[to] = append(reverse[to], from)
+	}
+	return graph, reverse
+}
--- boj/main.go
+++ boj/main.go
@@ -3,15 +3,8 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
-	"strings"
-)
-
-var (
-	areas, edges, input int
-	reader              = bufio.NewReader(os.Stdin)
-	ans                 = strings.Builder{}
-	visit               = [2][100001]bool{}
 )
 
 func main() {
@@ -19,65 +12,76 @@ func main() {
 }
 
 /**
+5 5
+1 3
+1 4
+4 5
+4 3
+3 2
 
-1번도시의 슈퍼히어로 미노만 이동 가능 첫번쨰 줄 에는 n,m 이 주어지고
-m 개의 줄에 걸쳐 x => y 로 이동할수 있는 도시의 번호를 주어진다 .
+3
 
-1. Create Graph with input values
 
-2. the start point is first and should move to 6
-
-3. so move theory twice
+임의의 두사람이 최소 몇단계 만에 이어질수 있는가 계산하는 법칙
+모든 사람에게 갈수 있는 경로를 파악하고 ++
 */
 
 func solution() {
+	var (
+		reader           = bufio.NewReader(os.Stdin)
+		users, relations int
+		index            int
+	)
 
-	fmt.Fscanln(reader, &areas, &edges)
-	graph, reverse := setGraph(areas, edges, reader)
-
-	findWay(1, areas, 0, graph)
-	findWay(areas, 1, 1, reverse)
-
-	fmt.Fscanln(reader, &input)
-	for i := 0; i < input; i++ {
-		var bomb int
-		fmt.Fscanln(reader, &bomb)
-		if visit[0][bomb] && visit[1][bomb] {
-			ans.WriteString("Defend the CTP\n")
-		} else {
-			ans.WriteString("Destroyed the CTP\n")
-		}
+	fmt.Fscanln(reader, &users, &relations)
+	graph := make([][]int, users+1)
+	answer := make([]int, users+1)
+	for i := range answer {
+		answer[i] = math.MaxInt
 	}
-	fmt.Println(ans.String())
-}
-func findWay(from, to, way int, graph map[int][]int) {
-	visit[way][from] = true
-	q := []int{from}
-	for len(q) > 0 {
-		cur := q[0]
-		q = q[1:]
-		for i := 0; i < len(graph[cur]); i++ {
-			next := graph[cur][i]
-			if visit[way][next] == false {
-				visit[way][next] = true
-				q = append(q, next)
-			}
-		}
-	}
-	return
-}
 
-func setGraph(areas, edges int, reader *bufio.Reader) (map[int][]int, map[int][]int) {
-	graph := make(map[int][]int)
-	reverse := make(map[int][]int)
-	for i := 0; i <= areas; i++ {
+	for i := range graph {
 		graph[i] = make([]int, 0)
 	}
-	for i := 0; i < edges; i++ {
-		var from, to int
-		fmt.Fscanln(reader, &from, &to)
-		graph[from] = append(graph[from], to)
-		reverse[to] = append(reverse[to], from)
+
+	for i := 0; i < relations; i++ {
+		var who, know int
+		fmt.Fscanln(reader, &who, &know)
+		graph[who] = append(graph[who], know)
+		graph[know] = append(graph[know], who)
 	}
-	return graph, reverse
+
+	for user := 1; user <= users; user++ {
+		var (
+			cnt   = 0
+			q     = []int{user}
+			visit = make([]bool, users+1)
+			run   = 0
+		)
+
+		visit[user] = true
+
+		for len(q) > 0 {
+			size := len(q)
+			for i := 0; i < size; i++ {
+				cur := q[0]
+				cnt += run
+				q = q[1:]
+				for _, v := range graph[cur] {
+					if visit[v] == false {
+						visit[v] = true
+						q = append(q, v)
+					}
+				}
+			}
+			run++
+		}
+
+		if answer[index] > cnt {
+			index = user
+			answer[index] = cnt
+		}
+	}
+
+	fmt.Println(index)
 }
