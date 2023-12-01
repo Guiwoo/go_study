da1c1a1c1849506c22d0354a96e4a7635d24bdf4
Guiwoo <park.guiwoo@hotmail.com>
2023-12-01 11:00:52 +0900

    boj 7562

--- boj/dfs_bfs/dfs_bfs.go
+++ boj/dfs_bfs/dfs_bfs.go
@@ -2,6 +2,7 @@ package dfs_bfs
 
 import (
 	"bufio"
+	"container/list"
 	"fmt"
 	"math"
 	"os"
@@ -1091,4 +1092,54 @@ func bfs13549(move, target int) {
 			}
 		}
 	}
+}
+
+var (
+	reader   = bufio.NewReader(os.Stdin)
+	writer   = bufio.NewWriter(os.Stdout)
+	from, to int
+
+	visit [100001]int
+	path  [100001]int
+)
+
+func solution13913() {
+
+	defer writer.Flush()
+	fmt.Fscan(reader, &from, &to)
+
+	q := list.New()
+
+	visit[from] = 1
+	path[from] = from
+	q.PushBack(from)
+
+	for q.Len() > 0 {
+		cur := q.Front().Value.(int)
+		q.Remove(q.Front())
+
+		if cur == to {
+			break
+		}
+		for _, v := range []int{-1, cur, 1} {
+			next := v + cur
+			if next >= 0 && next <= 100000 && visit[next] == 0 {
+				visit[next] = visit[cur] + 1
+				path[next] = cur
+				q.PushBack(next)
+			}
+		}
+	}
+
+	fmt.Fprintln(writer, visit[to]-1)
+	getPath(to)
+}
+
+func getPath(u int) {
+	if u == path[u] {
+		fmt.Fprint(writer, u, " ")
+		return
+	}
+	getPath(path[u])
+	fmt.Fprint(writer, u, " ")
 }
--- boj/main.go
+++ boj/main.go
@@ -7,56 +7,94 @@ import (
 	"os"
 )
 
+/**
+
+3
+8
+0 0
+7 0
+100
+0 0
+30 50
+10
+1 1
+1 1
+
+
+5
+28
+0
+*/
+
 func main() {
-	solution13913()
+	solution7562()
 }
 
-var (
-	reader   = bufio.NewReader(os.Stdin)
-	writer   = bufio.NewWriter(os.Stdout)
-	from, to int
-
-	visit [100001]int
-	path  [100001]int
-)
-
-func solution13913() {
-
+func solution7562() {
+	var (
+		reader = bufio.NewReader(os.Stdin)
+		writer = bufio.NewWriter(os.Stdout)
+		total  int
+	)
 	defer writer.Flush()
-	fmt.Fscan(reader, &from, &to)
 
-	q := list.New()
-
-	visit[from] = 1
-	path[from] = from
-	q.PushBack(from)
-
-	for q.Len() > 0 {
-		cur := q.Front().Value.(int)
-		q.Remove(q.Front())
-
-		if cur == to {
-			break
+	fmt.Fscanln(reader, &total)
+	for i := 0; i < total; i++ {
+		size, cur, target := getInput(reader)
+		if checkEqual(cur, target) {
+			fmt.Fprintln(writer, 0)
+			continue
 		}
-		for _, v := range []int{-1, cur, 1} {
-			next := v + cur
-			if next >= 0 && next <= 100000 && visit[next] == 0 {
-				visit[next] = visit[cur] + 1
-				path[next] = cur
-				q.PushBack(next)
+		minSteps := bfs7562(size, cur, target)
+		fmt.Fprintln(writer, minSteps)
+	}
+}
+
+func bfs7562(size int, cur, target []int) int {
+	answer := 0
+	dirs := [][]int{{-2, 1}, {-2, -1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}}
+	board := make([][]int, size)
+	for i := range board {
+		board[i] = make([]int, size)
+	}
+	q := list.New()
+	board[cur[0]][cur[1]] = -1
+	q.PushBack(cur)
+	for q.Len() > 0 {
+		qSize := q.Len()
+		for i := 0; i < qSize; i++ {
+			c := q.Front()
+			current := c.Value.([]int)
+			q.Remove(c)
+			for j := 0; j < len(dirs); j++ {
+				row := current[0] + dirs[j][0]
+				col := current[1] + dirs[j][1]
+
+				if row < 0 || col < 0 || row >= size || col >= size || board[row][col] == -1 {
+					continue
+				}
+				if checkEqual([]int{row, col}, target) {
+					return answer + 1
+				}
+				board[row][col] = -1
+				q.PushBack([]int{row, col})
 			}
 		}
+		answer++
 	}
-
-	fmt.Fprintln(writer, visit[to]-1)
-	getPath(to)
+	return answer
+}
+func checkEqual(a, b []int) bool {
+	return a[0] == b[0] && a[1] == b[1]
 }
 
-func getPath(u int) {
-	if u == path[u] {
-		fmt.Fprint(writer, u, " ")
-		return
-	}
-	getPath(path[u])
-	fmt.Fprint(writer, u, " ")
+func getInput(reader *bufio.Reader) (int, []int, []int) {
+	var (
+		size, curX, curY, targetX, targetY int
+	)
+	fmt.Fscanln(reader, &size)
+	fmt.Fscanln(reader, &curX, &curY)
+	fmt.Fscanln(reader, &targetX, &targetY)
+
+	return size, []int{curX, curY}, []int{targetX, targetY}
 }
