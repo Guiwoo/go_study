b261abe6b6140e805e4a38623cbae2e1186420ba
Guiwoo <park.guiwoo@hotmail.com>
2023-12-05 10:30:13 +0900

    boj 16948

--- boj/dfs_bfs/dfs_bfs.go
+++ boj/dfs_bfs/dfs_bfs.go
@@ -1164,6 +1164,10 @@ func solution7562() {
 	}
 }
 
+type Position struct {
+	row, col int
+}
+
 func bfs7562(size int, cur, target []int) int {
 	answer := 0
 	dirs := [][]int{{-2, 1}, {-2, -1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}}
@@ -1211,4 +1215,69 @@ func getInput(reader *bufio.Reader) (int, []int, []int) {
 	fmt.Fscanln(reader, &targetX, &targetY)
 
 	return size, []int{curX, curY}, []int{targetX, targetY}
+}
+
+func solution15558() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var length, jump int
+	fmt.Fscanln(reader, &length, &jump)
+
+	arr := setMap(reader, length)
+	visit := make([][]bool, 2)
+	for i := range visit {
+		visit[i] = make([]bool, length)
+	}
+
+	q := list.New()
+	q.PushBack(Position{0, 0}) // 0 left 1 right
+	visit[0][0] = true
+
+	sec := 0
+	for q.Len() > 0 {
+		size := q.Len()
+		for i := 0; i < size; i++ {
+			cur := q.Remove(q.Front()).(Position)
+			if cur.col >= length {
+				fmt.Fprintln(writer, 1)
+				return
+			}
+			for _, v := range []int{1, -1, jump} {
+				nextIdx := cur.col + v
+				if nextIdx >= length {
+					fmt.Fprintln(writer, 1)
+					return
+				}
+				if v == jump {
+					// 건너뛴 경우
+					otherRow := 1 - cur.row
+					if arr[otherRow][nextIdx] == 1 && !visit[otherRow][nextIdx] {
+						visit[otherRow][nextIdx] = true
+						q.PushBack(Position{otherRow, nextIdx})
+					}
+				} else if nextIdx > sec && arr[cur.row][nextIdx] == 1 && !visit[cur.row][nextIdx] {
+					visit[cur.row][nextIdx] = true
+					q.PushBack(Position{cur.row, nextIdx})
+				}
+			}
+		}
+		sec++
+	}
+	fmt.Fprintln(writer, 0)
+}
+
+func setMap(reader *bufio.Reader, length int) [][]int {
+	arr := make([][]int, 2)
+	for k := range arr {
+		sub := make([]int, length)
+		var num string
+		fmt.Fscanln(reader, &num)
+		for i, v := range num {
+			sub[i], _ = strconv.Atoi(string(v))
+		}
+		arr[k] = sub
+	}
+	return arr
 }
--- boj/main.go
+++ boj/main.go
@@ -5,78 +5,87 @@ import (
 	"container/list"
 	"fmt"
 	"os"
-	"strconv"
 )
 
-type Position struct {
-	row, col int
-}
-
 func main() {
-	solution15558()
+	solution16948()
 }
 
-func solution15558() {
-	reader := bufio.NewReader(os.Stdin)
-	writer := bufio.NewWriter(os.Stdout)
+/**
+r, c)라면, (r-2, c-1), (r-2, c+1), (r, c-2), (r, c+2), (r+2, c-1), (r+2, c+1)로
+첫째 줄에 데스 나이트가 (r1, c1)에서 (r2, c2)로 이동하는 최소 이동 횟수를 출력한다. 이동할 수 없는 경우에는 -1을 출력한다.
+
+7
+6 6 0 1
+
+4
+
+6
+5 1 0 5
+
+-1
+
+7
+0 3 4 3
+
+2
+*/
+
+func solution16948() {
+	var (
+		reader               = bufio.NewReader(os.Stdin)
+		writer               = bufio.NewWriter(os.Stdout)
+		size, steps          int
+		startRow, startCol   int
+		targetRow, targetCol int
+		dirs                 = [][]int{{-2, -1}, {-2, +1}, {0, -2}, {0, 2}, {2, -1}, {2, 1}}
+	)
 	defer writer.Flush()
 
-	var length, jump int
-	fmt.Fscanln(reader, &length, &jump)
+	fmt.Fscanln(reader, &size)
 
-	arr := setMap(reader, length)
-	visit := make([][]bool, 2)
-	for i := range visit {
-		visit[i] = make([]bool, length)
+	arr := make([][]int, size)
+
+	for i := range arr {
+		arr[i] = make([]int, size)
 	}
+
+	fmt.Fscanln(reader, &startRow, &startCol, &targetRow, &targetCol)
 
 	q := list.New()
-	q.PushBack(Position{0, 0}) // 0 left 1 right
-	visit[0][0] = true
+	arr[startRow][startCol] = -1
+	q.PushBack([]int{startRow, startCol})
 
-	sec := 0
 	for q.Len() > 0 {
-		size := q.Len()
-		for i := 0; i < size; i++ {
-			cur := q.Remove(q.Front()).(Position)
-			if cur.col >= length {
-				fmt.Fprintln(writer, 1)
+		length := q.Len()
+		for i := 0; i < length; i++ {
+			value := q.Front()
+			q.Remove(value)
+			cur := value.Value.([]int)
+
+			if cur[0] == targetRow && cur[1] == targetCol {
+				fmt.Fprintln(writer, steps)
 				return
 			}
-			for _, v := range []int{1, -1, jump} {
-				nextIdx := cur.col + v
-				if nextIdx >= length {
-					fmt.Fprintln(writer, 1)
+
+			for _, v := range dirs {
+				nextRow := cur[0] + v[0]
+				nextCol := cur[1] + v[1]
+
+				if nextRow < 0 || nextCol < 0 || nextRow >= size || nextCol >= size || arr[nextRow][nextCol] == -1 {
+					continue
+				}
+
+				arr[nextRow][nextCol] = -1
+				if nextRow == targetRow && nextCol == targetCol {
+					fmt.Fprintln(writer, steps+1)
 					return
 				}
-				if v == jump {
-					// 건너뛴 경우
-					otherRow := 1 - cur.row
-					if arr[otherRow][nextIdx] == 1 && !visit[otherRow][nextIdx] {
-						visit[otherRow][nextIdx] = true
-						q.PushBack(Position{otherRow, nextIdx})
-					}
-				} else if nextIdx > sec && arr[cur.row][nextIdx] == 1 && !visit[cur.row][nextIdx] {
-					visit[cur.row][nextIdx] = true
-					q.PushBack(Position{cur.row, nextIdx})
-				}
+				q.PushBack([]int{nextRow, nextCol})
 			}
 		}
-		sec++
+		steps++
 	}
-	fmt.Fprintln(writer, 0)
-}
 
-func setMap(reader *bufio.Reader, length int) [][]int {
-	arr := make([][]int, 2)
-	for k := range arr {
-		sub := make([]int, length)
-		var num string
-		fmt.Fscanln(reader, &num)
-		for i, v := range num {
-			sub[i], _ = strconv.Atoi(string(v))
-		}
-		arr[k] = sub
-	}
-	return arr
+	fmt.Fprintln(writer, -1)
 }
