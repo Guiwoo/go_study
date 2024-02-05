eea86f122ad5026d61c9e472a3817d5c7e84a9aa
Guiwoo <park.guiwoo@hotmail.com>
2024-02-05 18:04:43 +0900

    test

--- api_checker/main.go
+++ api_checker/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"github.com/google/uuid"
 	"io"
+	"log"
 	"mime/multipart"
 	"net/http"
 	"net/url"
@@ -54,41 +55,30 @@ func papagoTest() {
 type DATA struct {
 	Format string `json:"format"`
 	Name   string `json:"name"`
+	Data   string `json:"data,omitempty"`
+	Url    string `json:"url,omitempty"`
 }
 
-func newfileUploadRequest(uri string, params map[string]string, paramName, path string) (*http.Request, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
+func getEnv(env string) string {
+	value, ok := os.LookupEnv(env)
+	if ok == false {
+		log.Fatalf("fail to get env %+v", env)
+		return ""
 	}
-	defer file.Close()
-
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-	part, err := writer.CreateFormFile(paramName, filepath.Base(path))
-	if err != nil {
-		return nil, err
-	}
-	_, err = io.Copy(part, file)
-
-	for key, val := range params {
-		_ = writer.WriteField(key, val)
-	}
-	err = writer.Close()
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := http.NewRequest("POST", uri, body)
-	req.Header.Set("Content-Type", writer.FormDataContentType())
-	return req, err
+	return value
 }
 
-func main() {
-	host := ""
-	file, _ := os.ReadFile("/Users/guiwoopark/Desktop/something.png")
+func getImgFileToBase64(path string) string {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	return base64.StdEncoding.EncodeToString(file)
+}
 
-	encodedString := base64.StdEncoding.EncodeToString(file)
+func ocrSendURL() {
+	host := getEnv("naver_url")
+	secret := getEnv("xor_secret")
 
 	data := map[string]interface{}{
 		"version":   "V2",
@@ -96,24 +86,25 @@ func main() {
 		"timestamp": time.Now().Nanosecond(),
 		"lang":      "ko",
 		"images": []DATA{
-			DATA{"png", "something"},
+			DATA{
+				Format: "png",
+				Name:   "test-guiwoo",
+				Url:    "https://user-images.githubusercontent.com/67041069/201728725-6611c514-e1a5-4d78-9060-8965be25fd1c.png",
+			},
 		},
-		"enableTableDetection": true,
+		"resultType": "string",
 	}
-	payload := map[string]interface{}{
-		"message": data,
-	}
-	js, _ := json.Marshal(&payload)
 
-	//req, err := http.NewRequest("POST", host, bytes.NewReader(js))
-	req, err := newfileUploadRequest(host)
+	js, _ := json.Marshal(&data)
+
+	req, err := http.NewRequest("POST", host, bytes.NewReader(js))
 	if err != nil {
 		fmt.Println("Error creating request:", err)
 		return
 	}
 
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("X-OCR-SECRET", "")
+	req.Header.Set("X-OCR-SECRET", secret)
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
@@ -130,4 +121,125 @@ func main() {
 	}
 
 	fmt.Println(string(body))
+}
+
+func ocrSendData() {
+	host := getEnv("naver_url")
+	secret := getEnv("xor_secret")
+
+	data := map[string]interface{}{
+		"version":   "V2",
+		"requestId": uuid.NewString(),
+		"timestamp": time.Now().Nanosecond(),
+		"lang":      "ko",
+		"images": []DATA{
+			DATA{
+				Format: "png",
+				Name:   "test-guiwoo",
+				Data:   getImgFileToBase64("/Users/guiwoopark/Desktop/something.png"),
+			},
+		},
+		"resultType": "string",
+	}
+
+	js, _ := json.Marshal(&data)
+
+	req, err := http.NewRequest("POST", host, bytes.NewReader(js))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-OCR-SECRET", secret)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+
+	fmt.Println(string(body))
+}
+
+type MultiPartFormData struct {
+	Version   string      `json:"version"`
+	RequestID string      `json:"requestId"`
+	TimeStamp int         `json:"timestamp"`
+	Images    []ImageData `json:"images"`
+}
+type ImageData struct {
+	Format string `json:"format"`
+	Name   string `json:"name"`
+}
+
+func ocrSendFile() {
+	host := getEnv("naver_url")
+	secret := getEnv("xor_secret")
+	fmt.Println(host, secret)
+
+	file, err := os.Open("/Users/guiwoopark/Desktop/something.png")
+	defer file.Close()
+	if err != nil {
+		log.Panicf("fail to open file %+v", err)
+	}
+	data := MultiPartFormData{
+		Version:   "V2",
+		RequestID: uuid.NewString(),
+		TimeStamp: time.Now().Nanosecond(),
+		Images: []ImageData{
+			{
+				Format: "png",
+				Name:   "test-guiwoo",
+			},
+		},
+	}
+	value, _ := json.Marshal(&data)
+
+	reqBody := bytes.Buffer{}
+	writer := multipart.NewWriter(&reqBody)
+	if err := writer.WriteField("message", string(value)); err != nil {
+		panic(err)
+	}
+	part, _ := writer.CreateFormFile("file", filepath.Base(file.Name()))
+	if _, err := io.Copy(part, file); err != nil {
+		panic(err)
+	}
+	writer.Close()
+
+	req, err := http.NewRequest("POST", host, &reqBody)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	req.Header.Set("X-OCR-SECRET", secret)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+	fmt.Println(string(body))
+}
+
+func main() {
+	ocrSendFile()
 }
--- ocr/main.go
+++ ocr/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/otiai10/gosseract/v2"
+)
 
+func main() {
+	client := gosseract.NewClient()
+	defer client.Close()
+	client.SetImage("/Users/guiwoopark/Desktop/something.png")
+	text, _ := client.Text()
+	fmt.Println(text)
 }
