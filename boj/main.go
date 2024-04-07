3118e0751eb09eb886d080cafe3e88c547930e94
Guiwoo Park <park.guiwoo@hotmail.com>
2024-04-07 14:14:58 +0900

    sync

--- boj/main.go
+++ boj/main.go
@@ -1,39 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"sort"
-)
-
 func main() {
-	var (
-		reader = bufio.NewReader(os.Stdin)
-		writer = bufio.NewWriter(os.Stdout)
-		N      int
-	)
-	fmt.Fscanln(reader, &N)
 
-	arr := make([]int, N)
-
-	for i := 0; i < N; i++ {
-		var x int
-		fmt.Fscan(reader, &x)
-		arr[i] = x
-	}
-
-	sort.Ints(arr)
-
-	dp, sum := make([]int, N), arr[0]
-	dp[0] = arr[0]
-
-	for i := 1; i < N; i++ {
-		dp[i] = arr[i] + dp[i-1]
-		sum += dp[i]
-
-	}
-
-	fmt.Fprintln(writer, sum)
-	// git test fix commit and check security
 }
